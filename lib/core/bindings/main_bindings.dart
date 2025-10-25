55ef04a1a50ecd9dc1d9b2e802a8dad3edfdb1bc
vojougae35 <vojougae35@gmail.com>
2025-10-25 05:49:42 +0000

    refactor: MainBindings로 메인 의존성 초기화

---
+++ lib/core/bindings/main_bindings.dart
@@ -0,0 +1,19 @@
+import 'package:allclearer/app/routes/my_route_observer.dart';
+import 'package:allclearer/app/services/ad_service.dart';
+import 'package:allclearer/app/services/preset_setting_service.dart';
+import 'package:allclearer/app/services/storage_service.dart';
+import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class MainBindings extends Bindings {
+  final SharedPreferences prefs;
+
+  MainBindings(this.prefs);
+
+  @override
+  void dependencies() {
+    Get.put(StorageService(prefs));
+    Get.put(PresetSettingService());
+    Get.put(AdService());
+  }
+}
